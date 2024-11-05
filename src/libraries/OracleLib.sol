e31ac0205ab71da4fad95791d19506ab99b230ec
notctrl <1838676259@qq.com>
2024-11-05 11:31:20 +0800

    updates

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { AggregatorV3Interface } from "../../lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "../../lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 /*
  * @title OracleLib
@@ -13,28 +13,33 @@ import { AggregatorV3Interface } from "../../lib/chainlink-brownie-contracts/con
  * So if the Chainlink network explodes and you have a lot of money locked in the protocol... too bad.
  */
 library OracleLib {
-    error OracleLib__StalePrice();
+    error OracleLib_StalePrice();
 
     uint256 private constant TIMEOUT = 3 hours;
 
-    function staleCheckLatestRoundData(AggregatorV3Interface chainlinkFeed)
-        public
-        view
-        returns (uint80, int256, uint256, uint256, uint80)
-    {
-        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
-            chainlinkFeed.latestRoundData();
+    function staleCheckLatestRoundData(
+        AggregatorV3Interface chainlinkFeed
+    ) public view returns (uint80, int256, uint256, uint256, uint80) {
+        (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        ) = chainlinkFeed.latestRoundData();
 
         if (updatedAt == 0 || answeredInRound < roundId) {
-            revert OracleLib__StalePrice();
+            revert OracleLib_StalePrice();
         }
         uint256 secondsSince = block.timestamp - updatedAt;
-        if (secondsSince > TIMEOUT) revert OracleLib__StalePrice();
+        if (secondsSince > TIMEOUT) revert OracleLib_StalePrice();
 
         return (roundId, answer, startedAt, updatedAt, answeredInRound);
     }
 
-    function getTimeout(AggregatorV3Interface /* chainlinkFeed */ ) public pure returns (uint256) {
+    function getTimeout(
+        AggregatorV3Interface /* chainlinkFeed */
+    ) public pure returns (uint256) {
         return TIMEOUT;
     }
 }
