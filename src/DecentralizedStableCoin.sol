00782368ac07f67e20d53c8cf7211e8a4dd95b4d
notctrl <97250108+notctrl3@users.noreply.github.com>
2024-12-19 16:19:25 +0800

    Update DecentralizedStableCoin.sol

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -34,7 +34,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * Relative Stability: Pegged to USD
  *
  * This is the contract meant to be governed by DSCEngine.
- * This contract is just the  ERC20 implementation of our stablecoin system.
+ * This contract is just the ERC20 implementation of our stablecoin system.
  */
 contract DecentralizedStableCoin is ERC20Burnable, Ownable{
     error DecentralizedStableCoin_MustBeMoreThanZero();
